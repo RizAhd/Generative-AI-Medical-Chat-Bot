f04938c86501b7578815358eaee2231265a2ddf0
Riflan Mohamed <rizlanahmd4545@gmail.com>
2025-02-22 23:16:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Medical ChatBot',
+    version='0.0.0',
+    author='Riflan Mohamed',
+    author_email='rizlanahmd4545@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
